7cbdb7b43f366b270cea464de5e001fcc1ec7516
NeerajNixon <neerajnixon2002@gmail.com>
2024-08-01 16:10:33 +0530

    trained and tested CNN and LSTM architectures for actor and critic

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import os
 import cv2
 import numpy as np
 
-def Write_to_file(Date, net_worth, filename='{}.txt'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))):
+def Write_to_file(Date, net_worth, filename='{}.txt'.format(datetime.now().strftime("%Y-%m-%d %H.%M.%S"))):
     for i in net_worth: 
         Date += " {}".format(i)
     #print(Date)
@@ -22,11 +22,12 @@ class TradingGraph:
     # A crypto trading visualization using matplotlib made to render custom prices which come in following way:
     # Date, Open, High, Low, Close, Volume, net_worth, trades
     # call render every step
-    def __init__(self, Render_range):
+    def __init__(self, Render_range, Show_reward=False):
         self.Volume = deque(maxlen=Render_range)
         self.net_worth = deque(maxlen=Render_range)
         self.render_data = deque(maxlen=Render_range)
         self.Render_range = Render_range
+        self.Show_reward = Show_reward
 
         # We are using the style ‘ggplot’
         plt.style.use('ggplot')
@@ -78,16 +79,29 @@ class TradingGraph:
         self.ax1.xaxis.set_major_formatter(self.date_format)
         self.fig.autofmt_xdate()
 
+        minimum = np.min(np.array(self.render_data)[:,1:])
+        maximum = np.max(np.array(self.render_data)[:,1:])
+        RANGE = maximum - minimum
+
         # sort sell and buy orders, put arrows in appropiate order positions
         for trade in trades:
             trade_date = mpl_dates.date2num([pd.to_datetime(trade['Date'])])[0]
             if trade_date in Date_Render_range:
                 if trade['type'] == 'buy':
-                    high_low = trade['Low']-10
+                    high_low = trade['Low'] - RANGE*0.02
+                    ycoords = trade['Low'] - RANGE*0.08
                     self.ax1.scatter(trade_date, high_low, c='green', label='green', s = 120, edgecolors='none', marker="^")
                 else:
-                    high_low = trade['High']+10
+                    high_low = trade['High'] + RANGE*0.02
+                    ycoords = trade['High'] + RANGE*0.06
                     self.ax1.scatter(trade_date, high_low, c='red', label='red', s = 120, edgecolors='none', marker="v")
+
+                if self.Show_reward:
+                    try:
+                        self.ax1.annotate('{0:.2f}'.format(trade['Reward']), (trade_date-0.02, high_low), xytext=(trade_date-0.02, ycoords),
+                                                   bbox=dict(boxstyle='round', fc='w', ec='k', lw=1), fontsize="small")
+                    except:
+                        pass
 
         # we need to set layers every step, because we are clearing subplots every step
         self.ax2.set_xlabel('Date')
@@ -99,11 +113,12 @@ class TradingGraph:
 
         """Display image with matplotlib - interrupting other tasks"""
         # Show the graph without blocking the rest of the program
-        #plt.show(block=False)
+        plt.show(block=False)
         # Necessary to view frames before they are unrendered
-        #plt.pause(0.001)
+        plt.pause(0.001)
 
         """Display image with OpenCV - no interruption"""
+        """
         # redraw the canvas
         self.fig.canvas.draw()
         # convert canvas to image
@@ -119,3 +134,4 @@ class TradingGraph:
         if cv2.waitKey(25) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             return
+        """
